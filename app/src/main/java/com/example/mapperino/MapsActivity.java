d3d82e5da927fe2b8acb6e19fcc6cb6c7ade988a
Giovanni Zwaan <0971271@hr.nl>
2019-03-11 11:40:20 +0100

    Test commit

--- app/src/main/java/com/example/mapperino/MapsActivity.java
+++ app/src/main/java/com/example/mapperino/MapsActivity.java
@@ -1,45 +1,174 @@
 package com.example.mapperino;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.PlaceDetectionClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.FusedLocationProviderClient;
+
+import android.location.Location;
+
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private final String TAG = "MapsActivity";
+    private final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+
+    private final float DEFAULT_ZOOM = 12.0f;
+    private final float DEFAULT_CITY_ZOOM = 6.0f;
+
     private GoogleMap googleMap;
+    private GeoDataClient geoDataClient;
+    private PlaceDetectionClient placeDetectionClient;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private Location lastKnown;
+
+    private boolean hasLocationPermision;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        geoDataClient = Places.getGeoDataClient(this, null);
+        placeDetectionClient = Places.getPlaceDetectionClient(this, null);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        getLocationPermission();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        this.googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        this.googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        updateLocationUI();
+        getDeviceLocation();
+
+        googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng position) {
+                Log.i(TAG, String.format("%f %f", position.latitude, position.longitude));
+                addMarker(position.latitude, position.longitude);
+            }
+        });
+    }
+
+    private void getLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            hasLocationPermision = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+    private void updateLocationUI() {
+        if (googleMap == null) {
+            return;
+        }
+
+        try {
+            if (hasLocationPermision) {
+                googleMap.setMyLocationEnabled(true);
+                googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                googleMap.setMyLocationEnabled(false);
+                googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+                lastKnown = null;
+                getLocationPermission();
+            }
+        } catch (SecurityException e) {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    private void getDeviceLocation() {
+        try {
+            if (hasLocationPermision) {
+                Task locationResult = fusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            lastKnown = task.getResult();
+
+                            if (lastKnown == null) {
+                                Log.i(TAG, String.format("lastKnown is null."));
+                                return;
+                            }
+
+                            Log.i(TAG, String.format("%f %f", lastKnown.getLatitude(), lastKnown.getLongitude()));
+                            googleMap.addMarker(new MarkerOptions().position(
+                                    new LatLng(lastKnown.getLatitude(), lastKnown.getLongitude())
+                            ));
+
+                            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(lastKnown.getLatitude(),
+                                            lastKnown.getLongitude()), DEFAULT_ZOOM
+                            ));
+                        } else {
+                            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(null, DEFAULT_ZOOM));
+                            googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e) {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        hasLocationPermision = false;
+
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    hasLocationPermision = true;
+                }
+            }
+        }
+
+        updateLocationUI();
+    }
+
+    public void addMarker(double latitude, double longitude) {
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)));
+    }
+
+    public void addMarker(double latitude, double longitude, String title) {
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(latitude, longitude)).title(title));
     }
 }
