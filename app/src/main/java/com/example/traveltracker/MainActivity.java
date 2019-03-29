383110b9c382db2a56e0e260cd0124c51fd4ca9c
Giovanni Zwaan <0971271@hr.nl>
2019-03-29 13:45:04 +0100

    init

---
+++ app/src/main/java/com/example/traveltracker/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.traveltracker;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+    private final String IMAGE_CAPTURE = "android.media.action.IMAGE_CAPTURE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+        }
+    }
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_travels:
+                            selectedFragment = new TravelsFragment();
+                            break;
+                        case R.id.nav_camera:
+                            Intent intent = new Intent(IMAGE_CAPTURE);
+                            startActivity(intent);
+                            return false;
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+}
